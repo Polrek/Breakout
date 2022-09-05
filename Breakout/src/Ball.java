938e0f1a54333ebaf13efe4be96812414e370bdc
Polrek <rhys.n.edlin@gmail.com>
2022-09-05 19:12:46 +1200

    Finished first round of TODO investigations in Ball and Breakout

--- Breakout/src/Ball.java
+++ Breakout/src/Ball.java
@@ -6,9 +6,9 @@ public class Ball extends Sprite {
 	
 	// Constructor
 	public Ball() {
-		// TODO: Set width to Settings.BALL_WIDTH
-		// TODO: Set width to Settings.BALL_HEIGHT
-		// TODO: Call resetPosition
+		setWidth(Settings.BALL_WIDTH); // Set width to Settings.BALL_WIDTH
+		setHeight(Settings.BALL_HEIGHT); // Set width to Settings.BALL_HEIGHT
+		resetPosition(); // Call resetPosition
 	}
 	
 	/**
@@ -17,45 +17,46 @@ public class Ball extends Sprite {
 	 */
 	public void resetPosition() {
 		setX(Settings.INITIAL_BALL_X);
-		// TODO: Set the balls y by using the INITIAL_BALL_Y (see above)
+		setY(Settings.INITIAL_BALL_Y);// Set the balls y by using the INITIAL_BALL_Y (see above)
 	}
 	
 	public void update() {
 		x += xVelocity;
-		// TODO: Increase the y variable by yVelocity (see above)
+		y += yVelocity; // Increase the y variable by yVelocity (see above)
 		
 		// Bounce off left side of screen
 		if(x <= 0) {
-			// TODO: Set x to 0 so it does not leave the screen
-			// TODO: Change the x velocity to make the ball go right
+			x = 0;// TODO: CHECK THIS: Set x to 0 so it does not leave the screen
+			setXVelocity(-getXVelocity()); // TODO: CHECK THIS: Change the x velocity to make the ball go right
 		}
 		
 		// Bounce off right side of screen
 		if(x >= Settings.WINDOW_WIDTH - Settings.BALL_WIDTH) {
-			// TODO: Set x to the right edge of the screen (see the above if condition)
-			// TODO: Change the x velocity to make the ball go left
+			x = Settings.WINDOW_WIDTH - Settings.BALL_WIDTH; // TODO: CHECK THIS: Set x to the right edge of the screen (see the above if condition)
+			setXVelocity(-getXVelocity()); // TODO: CHECK THIS: Change the x velocity to make the ball go left 
+			//reverses the velocity of the ball "bounce"
 		}
 		
 		// Bounce off top of screen
 		if(y <= 0) {
-			// TODO: Set y to 0 so it does not leave the screen
-			// TODO: Change the y velocity to make the ball go downward
+			y = 0;// TODO: Set y to 0 so it does not leave the screen
+			setYVelocity(-getYVelocity());// TODO: CHECK THIS: Change the y velocity to make the ball go downward
 		}
 		
 	}
 	
 	public void setXVelocity(int x) {
-		// TODO: Set the x velocity
+		this.x = xVelocity;// TODO: CHECK THIS: Set the x velocity
 	}
 	public void setYVelocity(int y) {
-		// TODO: Set the y velocity
+		this.y = yVelocity;// TODO: CHECK THIS: Set the y velocity
 	}
 	
 	public int getXVelocity() {
-		return 0;	// TODO: Return the x velocity
+		return xVelocity;	// Return the x velocity
 	}
 	public int getYVelocity() {
-		return 0;	// TODO: Return the y velocity
+		return yVelocity;	//  Return the y velocity
 	}
 	
 	public void paint(Graphics g) {
--- Breakout/src/Breakout.java
+++ Breakout/src/Breakout.java
@@ -9,14 +9,14 @@ public class Breakout extends JFrame{
 	private BreakoutPanel panel;
 	
 	public Breakout() {
-		// TODO: Set the size of the screen (use Settings.WINDOW_WIDTH/HEIGHT)
-		// TODO: Set the title
-		// TODO: Set the background colour to white
-		// TODO: Set resizable to false
+		setSize(Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT); //Set the size of the screen
+		setTitle(Settings.WINDOW_NAME); //Set the title
+		setBackground(Color.WHITE);// Set the background colour to white
+		setResizable(false);// Set resizable to false
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         panel = new BreakoutPanel(this);
         add(panel);
-		// TODO: Set visible to true
+        setVisible(true); //Set visible to true
 	}
 
 	public static void main(String[] args) {
